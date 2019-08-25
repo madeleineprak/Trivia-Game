2622a713822c51981f9966e5e030e96105aaf809
Madeleine Prak <madeleineprak@gmail.com>
2019-08-24 22:13:53 -0700

    Added more vines and README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Trivia-Game
+This is a Vine Trivia Game. Click [here](https://madeleineprak.github.io/Trivia-Game/) to play it.
+
+Tools utilized:
+* HTML
+* CSS
+* Bootstrap (See documentation [here](https://getbootstrap.com/).)
+* JavaScript
+* jQuery 
+
+# How To Play
+Fill in the blank from the popular vine. Try and see how many you can get correct. Press the play button below to begin.
+
+# Authors
+[Madeleine Prak](https://github.com/madeleineprak)
+
+# Acknowledgements
+Inspiration came from [Vine](https://vine.co/).
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Sets up variables for game.
 var numCorrect = 0;
 var numWrong = 0;
 var numNotAnswered = 0;
@@ -5,6 +6,7 @@ var vinesArray = [];
 var currentQuestionIndex = 0;
 var time = 0;
 var intervalId = 0;
+// Populates the vine array with chosen vines below.
 function addVine(vineSentence, vineAnswer, vinePossibleAnswers, vinePhoto, vineGif) {
     var vineObject = { sentence: "", answer: "", possibleAnswers: [], photo: "", gif: ""};
     vineObject.sentence = vineSentence;
@@ -14,6 +16,7 @@ function addVine(vineSentence, vineAnswer, vinePossibleAnswers, vinePhoto, vineG
     vineObject.gif = vineGif;
     vinesArray.push(vineObject);
 }
+// Sets a timer for each question.
 function setTimer() {
     time = 10;
     $("#timer").html("<h6>Time: " + time + "</h6>");   
@@ -22,22 +25,24 @@ function setTimer() {
         if (time >= 0) {
             $("#timer").html("<h6>Time: " + time + "</h6>");
         }
-        if (time === -1) { // -1 to make sure it counts 0
+        if (time === -1) { // makes sure it counts 0
             clearInterval(intervalId);
             numNotAnswered++;
             showQuestionResult("Times up!");
         }
     }, 1000);
 };
+// Sets up with game with a timer and the current question.
 function setGame() {
     $("#instructions").hide();
     $("#start").hide();
-    // $("#footer").text("Madeleine Prak 2019.");
+    // $("#footer").text("Madeleine Prak 2019."); Going to fix this later.
     $("#quiz").show();
     $("#result").html("");
     setTimer();
     displayQuestion(currentQuestionIndex);
 }
+// Retrieves from the vine array and sets up the current question.
 function displayQuestion(currentQuestionIndex) {
     $("#answers").html("");
     $("#question").html("<h3>" + vinesArray[currentQuestionIndex].sentence + "</h3>");
@@ -46,13 +51,13 @@ function displayQuestion(currentQuestionIndex) {
     for (i = 0; i < vinesArray[currentQuestionIndex].possibleAnswers.length; i++) {
         //Creates a button for each answer
         var answerButton = $("<button>").text(vinesArray[currentQuestionIndex].possibleAnswers[i]);
-        // Adds indentifier to button
         answerButton.addClass("answer-button"); 
         answerButton.attr("data-number", i + 1);
-        // Adds the buttons to the answers div for the user to select from
+        // Adds the buttons
         $("#answers").append(answerButton);
     }
 }
+// Gets called after user chooses answer for each question.
 function showQuestionResult(outcome) {
     $("#timer").html("");
     $("#photo").attr("src", vinesArray[currentQuestionIndex].gif);
@@ -67,12 +72,15 @@ function showQuestionResult(outcome) {
         setTimeout(showQuizResult, 5000);
     }
 }
+// Gets called at the end when all questions have been answered.
 function showQuizResult(){
+    var totalVines = vinesArray.length;
     $("#quiz").hide();
     // $("#footer").text("");
     $("#start").show();
-    $("#result").html("<h4>Nice! You got " + numCorrect + " correct, " + numWrong + " wrong and " + numNotAnswered + " not answered.</h4>");
+    $("#result").html("<h4>Nice! You got " + numCorrect + " correct, " + numWrong + " wrong and " + numNotAnswered + " not answered out of " + totalVines +" total.</h4>");
 }
+// Registers which answer the user chooses and outputs if it is right or wrong accordingly.
 $(document).on("click", ".answer-button", function () {
     var playerAnswer = $(this).text();
     var correctAnswer = vinesArray[currentQuestionIndex].answer;
@@ -85,7 +93,15 @@ $(document).on("click", ".answer-button", function () {
         showQuestionResult("Wrong!");
     }
 });
-addVine("\"______.\"", "ok", ["hello", "ok", "how are you", "hey"], "assets/images/ok.jpg", "assets/images/ok.gif");
+// Adds all the vines.
+addVine("\"______.\"", "ok", ["hello", "O.K.", "oi", "hey"], "assets/images/ok.jpg", "assets/images/ok.gif");
 addVine("\"I smell like ______.\"", "beef", ["beef", "potatos", "flowers", "berries"], "assets/images/beef.jpg", "assets/images/beef.gif");
 addVine("\"______.\"", "wow", ["cool", "whoa", "rad", "wow"], "assets/images/wow.jpg", "assets/images/wow.gif");
 addVine("\"Look at all those ______!\"", "chickens", ["ducks", "geese", "birds", "chickens"], "assets/images/chickens.jpg", "assets/images/chickens.gif");
+addVine("\"You almost made me drop my ______!\"", "croissant", ["croissant", "toast", "plate", "bread"], "assets/images/croissant.jpg", "assets/images/croissant.gif");
+addVine("\"______ James.\"", "Lebron", ["Kevin", "Rick", "Lennie", "Lebron"], "assets/images/lebron.jpg", "assets/images/lebron.gif");
+addVine("\"Oh my god. I love ______. ______'s my life.\"", "Chipotle", ["Chick-Fil-A", "Taco Bell", "Pizza Hut", "Chipotle"], "assets/images/chipotle.jpg", "assets/images/chipotle.gif");
+addVine("Who is this boy?", "Gavin", ["Liam","Logan","Noah","Gavin"], "assets/images/gavin.jpg", "assets/images/gavin.gif");
+addVine("______!!!", "Lauren", ["Carole", "Kate", "Lauren", "Stacy"], "assets/images/mcflurry.jpg", "assets/images/lauren.gif");
+addVine("\"Who was the hottest ______ driver you've ever hard?\"", "Uber", ["Uber", "Lyft", "bus","taxi"], "assets/images/uber.jpg", "assets/images/uber.gif");
+addVine("\"______!\"", "Hzzzzaaaah", ["Ahhhhhh","Skrrrrrrt","Eeeeeeek","Hzzzzaaaah"], "assets/images/rat.jpg", "assets/images/rat.gif");
